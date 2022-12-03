e691bf211a6c6f57234fc2cb19a33e2429d73918
Biswas <rajdeep.biswas@sap.com>
2022-12-03 13:07:52 +0530

    commented first one

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,14 +1,21 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        # using the first string as a base since the longest possible common prefix cannot be longer than the shortest string!
         base = strs[0]
         longest = ''
+
+        # iterating through each letter
         for i in range(len(base)):
             found = True
+
+            # checking if said letter occurs at each the desired index for each string (besides the first one)
             for st in strs[1:]:
                 if i >= len(st) or st[i] != base[i]:
                     found = False
+
             if found:
                 longest += base[i]
             else:
+                # if even one match fails, it's incorrect to keep looking further, let alone inefficient!
                 break
         return longest
