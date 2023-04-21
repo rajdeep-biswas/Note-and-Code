4345ef2eed57f277864674e3af179b909ffb5c83
Biswas <rajdeep.biswas@sap.com>
2023-04-21 10:52:05 +0530

    rewrote using enumerate

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -3,14 +3,14 @@ class Solution:
 
         # dictionary to store each array element and its index
         checks = dict()
-        for i in range(len(nums)):
+        for i, ele in enumerate(nums):
 
             # find out what is the *other required number* that would complete the required sum
-            req = target - nums[i]
+            req = target - ele
 
             # check if said *other required number* is in the dictionary already, with that, return the current index and the other index
             if req in checks:
                 return [i, checks[req]]
             # if not, store current number as key and index as value
             else:
-                checks[nums[i]] = i
+                checks[ele] = i
