0cd1216f3a021a1da3e4c45e3d7ff4eaca49b272
Biswas <rajdeep.biswas@sap.com>
2023-04-26 22:36:14 +0530

    added leetcode 347

---
+++ Leetcode/347. Top K Frequent Elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        # conventional frequency hashmap
+        freq_dict = dict()
+        for num in nums:
+            freq_dict[num] = 1 + freq_dict.get(num, 0)
+
+        # counter array the same length as original array.
+        # note that it's important to initialize sublists in this manner instead of [[]] * (len(nums) + 1): check TIL 26th Apr Python
+        count = [[] for i in range(len(nums) + 1)]
+
+        # index will serve as the frequency and item will be sub-lists of all items that occur at that frequency
+        for key in freq_dict:
+            count[freq_dict[key]].append(key)
+
+        # results array populated in reverse, loop exits as soon as length of res expands to match k - which is the desired number of _most frequent elements_
+        res = []
+        for i in range(len(count) - 1, 0, -1):
+            for item in count[i]:
+                res.append(item)
+                if len(res) == k:
+                    return res
