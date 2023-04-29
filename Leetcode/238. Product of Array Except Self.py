af1d5a9e596689caed25217d8f878b1afaf1b191
Biswas <rajdeep.biswas@sap.com>
2023-04-29 16:31:35 +0530

    added comment to 238

--- Leetcode/238. Product of Array Except Self.py
+++ Leetcode/238. Product of Array Except Self.py
@@ -12,6 +12,7 @@ class Solution:
         for i in range(len(nums) - 1, -1, -1):
             postfix[i] = (postfix[i + 1] if i < len(nums) - 1 else 1) * nums[i]
 
+        # if it's confusing why this works - think of one solution that you had thought initially - what if you keep a cumulative muliplication results array - but the problem was there was no way to get the value of the "right-array-part", the postfix array solves that and lets you get results
         for i in range(len(nums)):
             nums[i] = (prefix[i - 1] if i > 0 else 1) * (postfix[i + 1] if i < len(nums) - 1 else 1)
 
