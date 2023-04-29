8b936c6b4d5024225ea54ffe58074b56f0b26556
Biswas <rajdeep.biswas@sap.com>
2023-04-29 16:23:17 +0530

    improved 238

--- Leetcode/238. Product of Array Except Self.py
+++ Leetcode/238. Product of Array Except Self.py
@@ -1,19 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        # my initial solution of computing total_product of array and just dividing by index location's number, and when the number is 0, just recalculating for that index
-        # pretty proud of it, but also sure it can be improved
-        total_prod = 1
-        for num in nums:
-            total_prod *= num
-        res = []
+        # O(n) improvement with neetcode guy's suggestion on using two different arrays
+        # the space complexity of this can indeed be further improved, he also said (https://www.youtube.com/watch?v=bNvIQI2wAjk)
+        prefix = [1] * len(nums)
+        postfix = [1] * len(nums)
+        
         for i in range(len(nums)):
-            if nums[i]:
-                res.append(total_prod // nums[i])
-            else:
-                mult = 1
-                for j in range(len(nums)):
-                    if i != j:
-                        mult *= nums[j]
-                res.append(mult)
-        return res
+            prefix[i] = (prefix[i - 1] if i > 0 else 1) * nums[i]
+        
+        for i in range(len(nums) - 1, -1, -1):
+            postfix[i] = (postfix[i + 1] if i < len(nums) - 1 else 1) * nums[i]
+
+        for i in range(len(nums)):
+            nums[i] = (prefix[i - 1] if i > 0 else 1) * (postfix[i + 1] if i < len(nums) - 1 else 1)
+
+        return nums
