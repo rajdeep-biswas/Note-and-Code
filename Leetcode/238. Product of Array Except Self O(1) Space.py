480f26c62c2a4656eee2be2b574939fbf1e2716d
Biswas <rajdeep.biswas@sap.com>
2023-05-05 22:45:51 +0530

    final optimization for 238

--- Leetcode/238. Product of Array Except Self O(1) Space.py
+++ Leetcode/238. Product of Array Except Self O(1) Space.py
@@ -1,17 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        # final optimization with O(1) space (ignoring the res array)
         res = [1] * len(nums)
         
         # prefix pass
         pre = 1
-        for i in range(len(nums) - 1):
-            res[i + 1] *= pre * nums[i]
+        for i in range(len(nums)):
+            res[i] = pre
             pre *= nums[i]
 
         # postfix pass
         post = 1
-        for i in range(len(nums) - 2, -1, -1):
-            res[i] *= post * nums[i + 1]
-            post *= nums[i + 1]
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= post
+            post *= nums[i]
 
         return res
