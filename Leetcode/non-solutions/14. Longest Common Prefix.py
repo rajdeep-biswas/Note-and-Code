3a0a29b307e8976d2041a7a5833519c5d1825e40
Biswas <rajdeep.biswas@sap.com>
2023-05-10 14:19:16 +0530

    added timeout solution for 14

--- Leetcode/non-solutions/14. Longest Common Prefix.py
+++ Leetcode/non-solutions/14. Longest Common Prefix.py
@@ -4,6 +4,7 @@ class Solution:
 
         # brute fore approach of checking every pair of walls
         for i in range(len(height)):
+            # note that there can be a slight optimization by doing j in range(i + 1, len(height)) but it doesn't help any of the testcases
             for j in range(len(height)):
                 # abs to get the horizontal distance (length / base) of the container
                 # min to get the lower of the two vertical distances (breadth / height) of the container. lower because the container can only hold water to that point
