0152f308ac9e12da2b09e700d8fe865c9ba587d5
Biswas <rajdeep.biswas@sap.com>
2023-05-10 14:14:47 +0530

    added timeout solution for 14

---
+++ Leetcode/non-solutions/14. Longest Common Prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = -1
+
+        # brute fore approach of checking every pair of walls
+        for i in range(len(height)):
+            for j in range(len(height)):
+                # abs to get the horizontal distance (length / base) of the container
+                # min to get the lower of the two vertical distances (breadth / height) of the container. lower because the container can only hold water to that point
+                # and simply updating max_area to higher value if found
+                max_area = max(max_area, abs(i - j) * min(height[i], height[j]))
+
+        return max_area
