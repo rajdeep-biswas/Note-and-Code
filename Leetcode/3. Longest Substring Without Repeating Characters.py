77e708b865039c0d3c7d8d1926c1e6ae26276d03
Biswas <rajdeep.biswas@sap.com>
2023-05-14 20:03:18 +0530

    added neetcode guy's solution to 3 which does seem a bit more readable

--- Leetcode/3. Longest Substring Without Repeating Characters.py
+++ Leetcode/3. Longest Substring Without Repeating Characters.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        if not s:
-            return 0
-        longest, longer = 0, 0
-        l, r = 0, 0
-        cur = set()
-        while r < len(s):
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        l = 0
+        res = 0
 
-            if s[r] not in cur:
-                longer += 1
-                cur.add(s[r])
-                r += 1
-            else:
-                longest = max(longest, longer)
-                longer = 0
-                cur = set()
+        for r in range(len(s)):
+
+            # while the rightmost found character exists as a duplicate in the set, keep removing from the left until it's removed.
+            # note that all elements to the left of the duplicate character will have to be removed in order to preserve the "subsequence-ness"
+            while s[r] in char_set:
+                char_set.remove(s[l])
                 l += 1
-                r = l
 
-        longest = max(longest, longer)
-        return longest
+            # this will take care of both cases of adding newly found character and adding back the duplicate character after all (one) occurence has been removed
+            char_set.add(s[r])
+
+            # set res to the gap between r and l
+            res = max(res, r - l + 1)
+
+        return res
