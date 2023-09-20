a4f8a6d68ae1d7f772ddf95768a9fcb19a7eafae
Biswas <rajdeep.biswas@sap.com>
2023-09-20 16:14:33 +0530

    added initial solution to 617

--- Leetcode/617. Merge Two Binary Trees (initial).py
+++ Leetcode/617. Merge Two Binary Trees (initial).py
@@ -7,23 +7,23 @@
 class Solution:
 
     # O(n) solution where I use dual-root DFS methods. I need two methods because doing it in the same method results in incorrectly summing nodes to itself *after* merging them (which should be just merged and not summed again)
- 
+
     # recursively adds values as long as correspnding nodes exist, pretty intuitive
     def addValues(self, node1, node2):
 
         # base case that checks both nodes exist (are not None)
         if not node1 and not node2:
             return None
-        
+
         # we use the first tree (node1) for the result (this is mentioned in the main method too)
         if node1 and node2:
             node1.val += node2.val
-        
+
         # recurse left and right as long as both exist
         if node1 and node2:
             self.addValues(node1.left, node2.left)
             self.addValues(node1.right, node2.right)
-    
+
 
     # recursively merges ("combines") None nodes in tree1 where corresponding nodes do exist in tree2
     def combineNodes(self, node1, node2):
@@ -31,15 +31,15 @@ class Solution:
         # base case that checks both nodes exist (are not None)
         if not node1 and not node2:
             return None
-        
+
         # check if both root node and their corresponding left nodes exist, otherwise it throws error. (a bit inelegant but works)
         if node1 and not node1.left and node2 and node2.left:
             node1.left = node2.left
-        
+
         # equivalent condition as above but for right nodes
         if node1 and not node1.right and node2 and node2.right:
             node1.right = node2.right
-        
+
         # recurse left and right as long as both exist
         if node1 and node2:
             self.combineNodes(node1.left, node2.left)
@@ -56,5 +56,5 @@ class Solution:
         # we add values *before* combining nodes because combining first would be incorrect because it'll be summing missing nodes with itself
         self.addValues(root1, root2)
         self.combineNodes(root1, root2)
-        
+
         return root1
