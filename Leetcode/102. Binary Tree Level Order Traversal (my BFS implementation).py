4e249e349d48e040e71f750d05cd3884f4935e27
Biswas <rajdeep.biswas@sap.com>
2023-09-21 20:24:11 +0530

    added comment to 102

--- Leetcode/102. Binary Tree Level Order Traversal (my BFS implementation).py
+++ Leetcode/102. Binary Tree Level Order Traversal (my BFS implementation).py
@@ -7,6 +7,7 @@
 class Solution:
 
     # BFS implementation with a tweak. inner loop captures same level elements in sub-lists instead of all elements going serially into a single list
+    # Update: found out that my implementation is the exact same as Neetcode's. The only difference is that he uses collections.deque() with which the .popleft() method comes with O(1) time but .insert(0, n) on a regular list (which is what I am doing) is actually O(n) time since it needs to shift all elements to the right. added to TIL
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
         # corner case of empty array being passed in. since we're inserting into a queue that will be [None] and loop does execute and it's a whole can of worms to not include this test, so better leave this here
