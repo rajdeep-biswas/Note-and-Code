21ac5345c1a51502d6ea0432b8f2f190979c309d
Biswas <rajdeep.biswas@sap.com>
2023-12-14 15:09:35 +0530

    added sol to 647

---
+++ Leetcode/647. Palindromic Substrings.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    # pretty much the same solution as Leetcode/5. Longest Palindromic Substring.py
+
+    def countPalindromes(self, s, l, r):
+        count = 0
+
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            count += 1
+            r += 1
+            l -= 1
+
+        return count
+
+    def countSubstrings(self, s: str) -> int:
+
+        odd_counts, even_counts = 0, 0
+        
+        for i in range(len(s)):
+
+            odd_counts += self.countPalindromes(s, i, i)
+            even_counts += self.countPalindromes(s, i, i + 1)
+        
+        return odd_counts + even_counts
