f1f93857fe663a4175098ed7a15404561f5c61d5
Biswas <rajdeep.biswas@sap.com>
2024-02-28 11:12:13 +0530

    commented 322

--- Leetcode/322. Coin Change (top down memoized).py
+++ Leetcode/322. Coin Change (top down memoized).py
@@ -7,24 +7,32 @@ class Solution:
 
     def bfs(self, coins, target):
 
+        # if subtracted amount (target minus coin) has reached zero, we have found a solution
         if target == 0:
             return 0
         
+        # if subtracted amount (target minus coin) has reached lower than zero, this is an invalid path
         if target < 0:
             return -1
 
+        # if target already exists in cache, return value
         if target in self.cache:
             return self.cache[target]
         
+
+        # looking for the smallest possibility at current level with all possible coins
         mini = float('inf')
 
         for coin in coins:
-            
+
+            # to get the "answer", we need to recursively look for all coin possibilities as well
             ans = self.bfs(coins, target - coin)
 
+            # if answer is zero or some value, we update "mini"mum found
             if ans != -1:
                 mini = min(mini, 1 + ans)
         
+        # we store smallest found value in cache for memoization
         self.cache[target] = mini
 
         return mini
