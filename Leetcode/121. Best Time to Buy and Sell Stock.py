665bbc8d6dc15297f82e9b83ec6fb4a8dccfbde8
Biswas <rajdeep.biswas@sap.com>
2024-04-23 14:05:47 +0530

    updated 121 with better comments and more readable code

--- Leetcode/121. Best Time to Buy and Sell Stock.py
+++ Leetcode/121. Best Time to Buy and Sell Stock.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        # https://www.youtube.com/watch?v=excAOvwF_Wk
-        max_profit = 0
-        min_ = prices[0]
+        # going to keep track of two values; max_profit_so_far, min_value_so_far
+
+        max_profit_so_far = 0
+        min_value_so_far = prices[0]
 
         for i in range(1, len(prices)):
-            max_profit = max(max_profit, prices[i] - min_)
-            min_ = min(min_, prices[i])
 
-        return max_profit
+            # and update each of the two at every step
+            # (it's not fully internalized in my brain how this exactly works)
+            max_profit_so_far = max(max_profit_so_far, prices[i] - min_value_so_far)
+            min_value_so_far = min(min_value_so_far, prices[i])
+        
+        return max_profit_so_far
